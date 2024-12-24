417bea96b91a08574106e22c485014a251c32abc
Yanlin Li <z5520011@ad.unsw.edu.au>
2024-12-24 22:16:42 +1100

    fixed tetro mobile controls movement

--- src/components/ActionButton.jsx
+++ src/components/ActionButton.jsx
@@ -1,7 +1,7 @@
 export const ActionButton = ({ onClick, text, color }) => {
   return (
     <button
-      className={`text-2xl font-bold text-center text-black bg-black rounded-full shadow-2xl size-10 ${color}`}
+      className={`text-2xl font-bold text-center text-black bg-black rounded-full shadow-2xl size-8 ${color}`}
       onClick={onClick}
     >
       {text}
